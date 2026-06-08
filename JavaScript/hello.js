c001abd17106c18014221a021c6defa1ab96007b
ktj <xowls0304@gmail.com>
2026-06-08 19:00:55 +0900

    20260608

---
+++ JavaScript/hello.js
@@ -0,0 +1,21 @@
+console.log("바카");
+console.error("아");
+console.error("아");
+/*
+console.error("아");
+console.error("아");
+console.error("아");
+console.error("아");
+console.error("아");
+console.error("아");
+console.warn("호");
+console.warn("호");
+*/
+console.warn("호");
+console.warn("호");
+console.warn("호");
+console.warn("호");
+console.warn("호");
+
+
+console.log("점수 : ", 90, "합격 : ", true);
